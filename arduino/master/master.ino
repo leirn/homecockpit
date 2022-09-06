dc5f8c9eee259ed3e08459784743354fe2d59c11
leirn <laurent@vromman.org>
2022-09-06 16:40:36 +0200

    Creation of protocol.h and wiring.h
    Wiring and Protocol are now defined in an external file to improve readibility and maintenability

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -6,118 +6,66 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 
-U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 6, /* data=*/ 5, /* cs=*/ 2, /* dc=*/ 3, /* reset=*/ 4);
+#include "wiring.h" // Pins and wiring definition
+#include "protocol.h" // Protocol message codes
+
+U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ SCREEN_CLOCK, /* data=*/ SCREEN_DATA, /* cs=*/ SCREEN_CS, /* dc=*/ SCREEN_DC, /* reset=*/ SCREEN_RESET);
 
 // Lights
 // BCN
 Bounce2::Button btn_lights_BCN = Bounce2::Button();
-#define BTN_BCN_PIN 33
-#define LED_BCN_PIN 32
-LED led_lights_BCN = LED(LED_BCN_PIN);
-#define EVENT_BCN_ON 0x1011
-#define EVENT_BCN_OFF 0x1010
+LED led_lights_BCN = LED(LED_BCN_PIN
 
 // LAND
 Bounce2::Button btn_lights_LAND = Bounce2::Button();
-#define BTN_LAND_PIN 35
-#define LED_LAND_PIN 34
 LED led_lights_LAND = LED(LED_LAND_PIN);
-#define EVENT_LAND_ON 0x1021
-#define EVENT_LAND_OFF 0x1020
 
 // TAXI
 Bounce2::Button btn_lights_TAXI = Bounce2::Button();
-#define BTN_TAXI_PIN 37
-#define LED_TAXI_PIN 36
 LED led_lights_TAXI = LED(LED_TAXI_PIN);
-#define EVENT_TAXI_ON 0x1031
-#define EVENT_TAXI_OFF 0x1030
 
 // NAV
 Bounce2::Button btn_lights_NAV = Bounce2::Button();
-#define BTN_NAV_PIN 39
-#define LED_NAV_PIN 38
 LED led_lights_NAV = LED(LED_NAV_PIN);
-#define EVENT_NAV_ON 0x1041
-#define EVENT_NAV_OFF 0x1040
 
 // Strobe
 Bounce2::Button btn_lights_STRB = Bounce2::Button();
-#define BTN_STRB_PIN 41
-#define LED_STRB_PIN 40
 LED led_lights_STRB = LED(LED_STRB_PIN);
-#define EVENT_STRB_ON 0x1051
-#define EVENT_STRB_OFF 0x1050
 
 
 // Engine and systems
 // Left Pump
 Bounce2::Button btn_engine_LPUMP = Bounce2::Button();
-#define BTN_LPUMP_PIN 23
-#define LED_LPUMP_PIN 22
 LED led_engine_LPUMP = LED(LED_LPUMP_PIN);
-#define EVENT_LPUMP_ON 0x2011
-#define EVENT_LPUMP_OFF 0x2010
 // Right Pump
 Bounce2::Button btn_engine_RPUMP = Bounce2::Button();
-#define BTN_RPUMP_PIN 25
-#define LED_RPUMP_PIN 24
 LED led_engine_RPUMP = LED(LED_RPUMP_PIN);
-#define EVENT_RPUMP_ON 0x2021
-#define EVENT_RPUMP_OFF 0x2020
 // Carb Head
 Bounce2::Button btn_engine_CARB = Bounce2::Button();
-#define BTN_CARB_PIN 27
-#define LED_CARB_PIN 26
 LED led_engine_CARB = LED(LED_CARB_PIN);
-#define EVENT_CARB_ON 0x2031
-#define EVENT_CARB_OFF 0x2030
 // Pitot Head
 Bounce2::Button btn_engine_PITOT = Bounce2::Button();
-#define BTN_PITOT_PIN 29
-#define LED_PITOT_PIN 28
 LED led_engine_PITOT = LED(LED_PITOT_PIN);
-#define EVENT_PITOT_ON 0x6011
-#define EVENT_PITOT_OFF 0x6010
 // Anti ice
 Bounce2::Button btn_engine_ANTI_ICE = Bounce2::Button();
-#define BTN_ANTI_ICE_PIN 31
-#define LED_ANTI_ICE_PIN 30
 LED led_engine_ANTI_ICE = LED(LED_ANTI_ICE_PIN);
-#define EVENT_ANTI_ICE_ON 0xffff
-#define EVENT_ANTI_ICE_OFF 0xffff
 
 // Landing gear
 Bounce2::Button btn_LDG_GEAR = Bounce2::Button();
-#define BTN_LDG_GEAR_PIN 46
-#define LED_LDG_GEAR_RED_PIN 42
 LED led_LDG_GEAR_RED = LED(LED_LDG_GEAR_RED_PIN);
-#define LED_LDG_GEAR_GREEN_LEFT_PIN 43
 LED led_LDG_GEAR_GREEN_LEFT = LED(LED_LDG_GEAR_GREEN_LEFT_PIN);
-#define LED_LDG_GEAR_GREEN_CENTER_PIN 44
 LED led_LDG_GEAR__GREEN_CENTER = LED(LED_LDG_GEAR_GREEN_CENTER_PIN);
-#define LED_LDG_GEAR_GREEN_RIGHT_PIN 45
 LED led_LDG_GEAR_GREEN_RIGHT = LED(LED_LDG_GEAR_GREEN_RIGHT_PIN);
-#define EVENT_LDG_GEAR_UP 0x6020
-#define EVENT_LDG_GEAR_DN 0x6021
 
 // Flaps
 Bounce2::Button btn_FLAPS_UP = Bounce2::Button();
-#define BTN_FLAPS_UP_PIN 51
 Bounce2::Button btn_FLAPS_DN = Bounce2::Button();
-#define BTN_FLAPS_DN_PIN 52
-#define LED_FLAPS_0_PIN 47
-#define LED_FLAPS_1_PIN 48
-#define LED_FLAPS_2_PIN 49
-#define LED_FLAPS_FULL_PIN 50
 LED led_FLAPS[4];
 led_FLAPS[0] = LED(LED_FLAPS_0_PIN);
 led_FLAPS[1] = LED(LED_FLAPS_1_PIN);
 led_FLAPS[2] = LED(LED_FLAPS_2_PIN);
 led_FLAPS[3] = LED(LED_FLAPS_FULL_PIN);
 int current_flaps = 0;
-#define EVENT_FLAPS_INC 0x6061
-#define EVENT_FLAPS_DEC 0x6060
 
 int update_display = 0;
 LOG_DEPTH = 8;
