f5649b53d06c98323cfc67d724325408436fb91e
leirn <laurent@vromman.org>
2022-09-06 15:16:17 +0200

    Adding LED.h usage

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -1,11 +1,13 @@
 #include <Wire.h> // I2C communications between arduinos
 #include <Bounce2.h> // For buttons and switch debouncing
+#include <LED.h> // To handle LEDs. Allows led.blink()
 
 // Lights
 // BCN
 Bounce2::Button btn_lights_BCN = Bounce2::Button();
 #define BTN_BCN_PIN 33
 #define LED_BCN_PIN 32
+LED led_lights_BCN = LED(LED_BCN_PIN);
 #define EVENT_BCN_ON 0x1011
 #define EVENT_BCN_OFF 0x1010
 
@@ -13,6 +15,7 @@ Bounce2::Button btn_lights_BCN = Bounce2::Button();
 Bounce2::Button btn_lights_LAND = Bounce2::Button();
 #define BTN_LAND_PIN 35
 #define LED_LAND_PIN 34
+LED led_lights_LAND = LED(LED_LAND_PIN);
 #define EVENT_LAND_ON 0x1021
 #define EVENT_LAND_OFF 0x1020
 
@@ -20,6 +23,7 @@ Bounce2::Button btn_lights_LAND = Bounce2::Button();
 Bounce2::Button btn_lights_TAXI = Bounce2::Button();
 #define BTN_TAXI_PIN 37
 #define LED_TAXI_PIN 36
+LED led_lights_TAXI = LED(LED_TAXI_PIN);
 #define EVENT_TAXI_ON 0x1031
 #define EVENT_TAXI_OFF 0x1030
 
@@ -27,6 +31,7 @@ Bounce2::Button btn_lights_TAXI = Bounce2::Button();
 Bounce2::Button btn_lights_NAV = Bounce2::Button();
 #define BTN_NAV_PIN 39
 #define LED_NAV_PIN 38
+LED led_lights_NAV = LED(LED_NAV_PIN);
 #define EVENT_NAV_ON 0x1041
 #define EVENT_NAV_OFF 0x1040
 
@@ -34,6 +39,7 @@ Bounce2::Button btn_lights_NAV = Bounce2::Button();
 Bounce2::Button btn_lights_STRB = Bounce2::Button();
 #define BTN_STRB_PIN 41
 #define LED_STRB_PIN 40
+LED led_lights_STRB = LED(LED_STRB_PIN);
 #define EVENT_STRB_ON 0x1051
 #define EVENT_STRB_OFF 0x1050
 
@@ -43,12 +49,14 @@ Bounce2::Button btn_lights_STRB = Bounce2::Button();
 Bounce2::Button btn_engine_LPUMP = Bounce2::Button();
 #define BTN_LPUMP_PIN 23
 #define LED_LPUMP_PIN 22
+LED led_engine_LPUMP = LED(LED_LPUMP_PIN);
 #define EVENT_LPUMP_ON 0x2011
 #define EVENT_LPUMP_OFF 0x2010
 // Right Pump
 Bounce2::Button btn_engine_RPUMP = Bounce2::Button();
 #define BTN_RPUMP_PIN 25
 #define LED_RPUMP_PIN 24
+LED led_engine_RPUMP = LED(LED_RPUMP_PIN);
 #define EVENT_RPUMP_ON 0x2021
 #define EVENT_RPUMP_OFF 0x2020
 
@@ -89,61 +97,61 @@ void loop()
     btn_lights_BCN.update();
     if (btn_lights_BCN.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_BCN_ON);
-        digitalWrite(LED_BCN_PIN, HIGH);
+        Serial.write(EVENT_BCN_OFF);
+        led_lights_BCN.off();
     }
     else if (btn_lights_BCN.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_BCN_OFF);
-        digitalWrite(LED_BCN_PIN, LOW);
+        Serial.write(EVENT_BCN_ON);
+        led_lights_BCN.on();
     }
     // LAND
     btn_lights_LAND.update();
     if (btn_lights_LAND.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_LAND_ON);
-        digitalWrite(LED_LAND_PIN, HIGH);
+        Serial.write(EVENT_LAND_OFF);
+        led_lights_LAND.off();
     }
     else if (btn_lights_LAND.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_LAND_OFF);
-        digitalWrite(LED_LAND_PIN, LOW);
+        Serial.write(EVENT_LAND_ON);
+        led_lights_LAND.on();
     }
     // TAXI
     btn_lights_TAXI.update();
     if (btn_lights_TAXI.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_TAXI_ON);
-        digitalWrite(LED_TAXI_PIN, HIGH);
+        Serial.write(EVENT_TAXI_OFF);
+        led_lights_TAXI.off();
     }
     else if (btn_lights_TAXI.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_TAXI_OFF);
-        digitalWrite(LED_TAXI_PIN, LOW);
+        Serial.write(EVENT_TAXI_ON);
+        led_lights_TAXI.on();
     }
     // NAV
     btn_lights_NAV.update();
     if (btn_lights_NAV.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_NAV_ON);
-        digitalWrite(LED_NAV_PIN, HIGH);
+        Serial.write(EVENT_NAV_OFF);
+        led_lights_NAV.off();
     }
     else if (btn_lights_NAV.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_NAV_OFF);
-        digitalWrite(LED_NAV_PIN, LOW);
+        Serial.write(EVENT_NAV_ON);
+        led_lights_NAV.on();
     }
     // Strobe
     btn_lights_STRB.update();
     if (btn_lights_STRB.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_STRB_ON);
-        digitalWrite(LED_STRB_PIN, HIGH);
+        Serial.write(EVENT_STRB_OFF);
+        led_lights_STRB.off();
     }
     else if (btn_lights_STRB.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_STRB_OFF);
-        digitalWrite(LED_STRB_PIN, LOW);
+        Serial.write(EVENT_STRB_ON);
+        led_lights_STRB.on();
     }
 
     // Engines
@@ -151,24 +159,24 @@ void loop()
     btn_engine_LPUMP.update();
     if (btn_engine_LPUMP.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_LPUMP_ON);
-        digitalWrite(LED_LPUMP_PIN, HIGH);
+        Serial.write(EVENT_LPUMP_OFF);
+        led_engine_LPUMP.off();
     }
     else if (btn_engine_LPUMP.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_LPUMP_OFF);
-        digitalWrite(LED_LPUMP_PIN, LOW);
+        Serial.write(EVENT_LPUMP_ON);
+        led_engine_LPUMP.on();
     }
     // Right Pump
     btn_engine_RPUMP.update();
     if (btn_engine_RPUMP.fell())
     { // Switch moved to OFF
-        Serial.write(EVENT_RPUMP_ON);
-        digitalWrite(LED_RPUMP_PIN, HIGH);
+        Serial.write(EVENT_RPUMP_OFF);
+        led_engine_LPUMP.off();
     }
     else if (btn_engine_RPUMP.rose())
     { // Switch moved to ON
-        Serial.write(EVENT_RPUMP_OFF);
-        digitalWrite(LED_RPUMP_PIN, LOW);
+        Serial.write(EVENT_RPUMP_ON);
+        led_engine_RPUMP.on();
     }
 }
