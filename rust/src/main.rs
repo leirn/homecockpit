68860cb3ec3c4baf1c1b0c8d0507e7961d8760a7
leirn <laurent@vromman.org>
2022-09-07 09:41:35 +0200

    Initiate COM handling

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,26 @@
+use serialport::*;
+
 fn main() {
     println!("Hello, world!");
+    
+    let ports = available_ports().expect("Failed to list serial ports");
+    
+    for port in ports {
+        println!("Port name : {}", port.port_name);
+    }
+    
+    let port = serialport::new("COM3", 9600).open().expect("Failed to open port");
+    
+    while true {
+        let mut buffer = [0; 2];
+        
+        bytes_to_read = port.bytes_to_read()?;
+        if (bytes_to_read > 1) {
+            port.read_exact(&mut buffer)?;
+            
+            let message:u16 = buffer[0] as u16 << 8 + buffer[1] as u16;
+            
+            
+        }
+    }
 }
