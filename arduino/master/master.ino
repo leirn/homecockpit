e804ba1efeadea82de521735229950e34a66455b
leirn <laurent@vromman.org>
2022-09-07 21:30:53 +0200

    Compilation error

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -14,7 +14,7 @@ U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ SCREEN_CLOCK, 
 // Lights
 // BCN
 Bounce2::Button btn_lights_BCN = Bounce2::Button();
-LED led_lights_BCN = LED(LED_BCN_PIN
+LED led_lights_BCN = LED(LED_BCN_PIN);
 
 // LAND
 Bounce2::Button btn_lights_LAND = Bounce2::Button();
@@ -54,278 +54,286 @@ LED led_engine_ANTI_ICE = LED(LED_ANTI_ICE_PIN);
 Bounce2::Button btn_LDG_GEAR = Bounce2::Button();
 LED led_LDG_GEAR_RED = LED(LED_LDG_GEAR_RED_PIN);
 LED led_LDG_GEAR_GREEN_LEFT = LED(LED_LDG_GEAR_GREEN_LEFT_PIN);
-LED led_LDG_GEAR__GREEN_CENTER = LED(LED_LDG_GEAR_GREEN_CENTER_PIN);
+LED led_LDG_GEAR_GREEN_CENTER = LED(LED_LDG_GEAR_GREEN_CENTER_PIN);
 LED led_LDG_GEAR_GREEN_RIGHT = LED(LED_LDG_GEAR_GREEN_RIGHT_PIN);
 
 // Flaps
 Bounce2::Button btn_FLAPS_UP = Bounce2::Button();
 Bounce2::Button btn_FLAPS_DN = Bounce2::Button();
-LED led_FLAPS[4];
-led_FLAPS[0] = LED(LED_FLAPS_0_PIN);
-led_FLAPS[1] = LED(LED_FLAPS_1_PIN);
-led_FLAPS[2] = LED(LED_FLAPS_2_PIN);
-led_FLAPS[3] = LED(LED_FLAPS_FULL_PIN);
+LED led_FLAPS[4] = {LED(LED_FLAPS_0_PIN), LED(LED_FLAPS_1_PIN), LED(LED_FLAPS_2_PIN), LED(LED_FLAPS_FULL_PIN)};
 int current_flaps = 0;
 
 int update_display = 0;
-LOG_DEPTH = 8;
+#define LOG_DEPTH 8
 String LOG_STACK[LOG_DEPTH];
+
+void logger(String log_string)
+{
+  update_display = 1;
+  for (int i = 0 ; i < LOG_DEPTH - 1 ; i ++)
+  {
+    LOG_STACK[i] = LOG_STACK[i + 1];
+  }
+  LOG_STACK[LOG_DEPTH - 1] = log_string;
+}
 
 void setup()
 {
-    for(i = 0; i < LOG_DEPTH - 1 ; i++)
-    {
-        LOG_STACK[0] = " ";
-    }
-    LOG_STACK[LOG_DEPTH - 1] = "Initialization started...";
-    
-    // Initializing screen
-    u8g2.begin();   
-    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    update_display = 1;
-    
-    Serial.begin(9600); // Init Serial interface
+  for (int i = 0; i < LOG_DEPTH - 1 ; ++i)
+  {
+    LOG_STACK[0] = " ";
+  }
+  LOG_STACK[LOG_DEPTH - 1] = "Initialization started...";
 
-    // Initiate lights
-    // BCN
-    btn_lights_BCN.attach(BTN_BCN_PIN, INPUT_PULLUP);
-    btn_lights_BCN.interval(25);
-    // LAND
-    btn_lights_LAND.attach(BTN_LAND_PIN, INPUT_PULLUP);
-    btn_lights_LAND.interval(25);
-    // TAXI
-    btn_lights_TAXI.attach(BTN_TAXI_PIN, INPUT_PULLUP);
-    btn_lights_TAXI.interval(25);
-    // NAV
-    btn_lights_NAV.attach(BTN_NAV_PIN, INPUT_PULLUP);
-    btn_lights_NAV.interval(25);
-    // Strobe
-    btn_lights_STRB.attach(BTN_STRB_PIN, INPUT_PULLUP);
-    btn_lights_STRB.interval(25);
+  // Initializing screen
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+  update_display = 1;
 
-    // Engine and systems
-    // Left Pump
-    btn_engine_LPUMP.attach(BTN_LPUMP_PIN, INPUT_PULLUP);
-    btn_engine_LPUMP.interval(25);
-    // Right Pump
-    btn_engine_RPUMP.attach(BTN_RPUMP_PIN, INPUT_PULLUP);
-    btn_engine_RPUMP.interval(25);
-    // Carb Heat
-    btn_engine_CARB.attach(BTN_CARB_PIN, INPUT_PULLUP);
-    btn_engine_CARB.interval(25);
-    // Pitot Heat
-    btn_engine_PITOT.attach(BTN_PITOT_PIN, INPUT_PULLUP);
-    btn_engine_PITOT.interval(25);
-    // Anti-ice
-    btn_engine_ANTI_ICE.attach(BTN_ANTI_ICE_PIN, INPUT_PULLUP);
-    btn_engine_ANTI_ICE.interval(25);
-    
-    // Landing Gear
-    btn_LDG_GEAR.attach(BTN_LDG_GEAR_PIN, INPUT_PULLUP);
-    btn_LDG_GEAR.interval(25);
-    
-    // Flaps
-    btn_FLAPS_UP.attach(BTN_FLAPS_UP_PIN, INPUT_PULLUP);
-    btn_FLAPS_UP.interval(25);
-    btn_FLAPS_DN.attach(BTN_FLAPS_DN_PIN, INPUT_PULLUP);
-    btn_FLAPS_DN.interval(25);
+  Serial.begin(9600); // Init Serial interface
+
+  // Initiate lights
+  // BCN
+  btn_lights_BCN.attach(BTN_BCN_PIN, INPUT_PULLUP);
+  btn_lights_BCN.interval(25);
+  // LAND
+  btn_lights_LAND.attach(BTN_LAND_PIN, INPUT_PULLUP);
+  btn_lights_LAND.interval(25);
+  // TAXI
+  btn_lights_TAXI.attach(BTN_TAXI_PIN, INPUT_PULLUP);
+  btn_lights_TAXI.interval(25);
+  // NAV
+  btn_lights_NAV.attach(BTN_NAV_PIN, INPUT_PULLUP);
+  btn_lights_NAV.interval(25);
+  // Strobe
+  btn_lights_STRB.attach(BTN_STRB_PIN, INPUT_PULLUP);
+  btn_lights_STRB.interval(25);
+
+  // Engine and systems
+  // Left Pump
+  btn_engine_LPUMP.attach(BTN_LPUMP_PIN, INPUT_PULLUP);
+  btn_engine_LPUMP.interval(25);
+  // Right Pump
+  btn_engine_RPUMP.attach(BTN_RPUMP_PIN, INPUT_PULLUP);
+  btn_engine_RPUMP.interval(25);
+  // Carb Heat
+  btn_engine_CARB.attach(BTN_CARB_PIN, INPUT_PULLUP);
+  btn_engine_CARB.interval(25);
+  // Pitot Heat
+  btn_engine_PITOT.attach(BTN_PITOT_PIN, INPUT_PULLUP);
+  btn_engine_PITOT.interval(25);
+  // Anti-ice
+  btn_engine_ANTI_ICE.attach(BTN_ANTI_ICE_PIN, INPUT_PULLUP);
+  btn_engine_ANTI_ICE.interval(25);
+
+  // Landing Gear
+  btn_LDG_GEAR.attach(BTN_LDG_GEAR_PIN, INPUT_PULLUP);
+  btn_LDG_GEAR.interval(25);
+
+  // Flaps
+  btn_FLAPS_UP.attach(BTN_FLAPS_UP_PIN, INPUT_PULLUP);
+  btn_FLAPS_UP.interval(25);
+  btn_FLAPS_DN.attach(BTN_FLAPS_DN_PIN, INPUT_PULLUP);
+  btn_FLAPS_DN.interval(25);
 }
 
 void loop()
 {
-    updateButtons();
-    readSerialInputFromComputer();
-    readSerialInputFromSlave();
-    updateDisplay();
+  updateButtons();
+  readSerialInputFromComputer();
+  readSerialInputFromSlave();
+  updateDisplay();
 }
 
-void updateButtons() {
-    // Lights
-    // BCN
-    btn_lights_BCN.update();
-    if (btn_lights_BCN.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_BCN_OFF);
-        led_lights_BCN.off();
-        log("BCN light set to OFF");
-    }
-    else if (btn_lights_BCN.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_BCN_ON);
-        led_lights_BCN.on();
-        log("BCN light set to ON");
-    }
-    // LAND
-    btn_lights_LAND.update();
-    if (btn_lights_LAND.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_LAND_OFF);
-        led_lights_LAND.off();
-        log("LAND light set to OFF");
-    }
-    else if (btn_lights_LAND.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_LAND_ON);
-        led_lights_LAND.on();
-        log("LAND light set to ON");
-    }
-    // TAXI
-    btn_lights_TAXI.update();
-    if (btn_lights_TAXI.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_TAXI_OFF);
-        led_lights_TAXI.off();
-        log("TAXI light set to OFF");
-    }
-    else if (btn_lights_TAXI.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_TAXI_ON);
-        led_lights_TAXI.on();
-        log("TAXI light set to ON");
-    }
-    // NAV
-    btn_lights_NAV.update();
-    if (btn_lights_NAV.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_NAV_OFF);
-        led_lights_NAV.off();
-        log("NAV light set to OFF");
-    }
-    else if (btn_lights_NAV.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_NAV_ON);
-        led_lights_NAV.on();
-        log("NAV light set to ON");
-    }
-    // Strobe
-    btn_lights_STRB.update();
-    if (btn_lights_STRB.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_STRB_OFF);
-        led_lights_STRB.off();
-        log("STRB light set to OFF");
-    }
-    else if (btn_lights_STRB.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_STRB_ON);
-        led_lights_STRB.on();
-        log("STRB light set to ON");
-    }
+void updateButtons()
+{
+  // Lights
+  // BCN
+  btn_lights_BCN.update();
+  if (btn_lights_BCN.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_BCN_OFF);
+    led_lights_BCN.off();
+    logger("BCN light set to OFF");
+  }
+  else if (btn_lights_BCN.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_BCN_ON);
+    led_lights_BCN.on();
+    logger("BCN light set to ON");
+  }
+  // LAND
+  btn_lights_LAND.update();
+  if (btn_lights_LAND.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_LAND_OFF);
+    led_lights_LAND.off();
+    logger("LAND light set to OFF");
+  }
+  else if (btn_lights_LAND.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_LAND_ON);
+    led_lights_LAND.on();
+    logger("LAND light set to ON");
+  }
+  // TAXI
+  btn_lights_TAXI.update();
+  if (btn_lights_TAXI.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_TAXI_OFF);
+    led_lights_TAXI.off();
+    logger("TAXI light set to OFF");
+  }
+  else if (btn_lights_TAXI.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_TAXI_ON);
+    led_lights_TAXI.on();
+    logger("TAXI light set to ON");
+  }
+  // NAV
+  btn_lights_NAV.update();
+  if (btn_lights_NAV.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_NAV_OFF);
+    led_lights_NAV.off();
+    logger("NAV light set to OFF");
+  }
+  else if (btn_lights_NAV.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_NAV_ON);
+    led_lights_NAV.on();
+    logger("NAV light set to ON");
+  }
+  // Strobe
+  btn_lights_STRB.update();
+  if (btn_lights_STRB.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_STRB_OFF);
+    led_lights_STRB.off();
+    logger("STRB light set to OFF");
+  }
+  else if (btn_lights_STRB.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_STRB_ON);
+    led_lights_STRB.on();
+    logger("STRB light set to ON");
+  }
 
-    // Engines
-    // Left Pump
-    btn_engine_LPUMP.update();
-    if (btn_engine_LPUMP.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_LPUMP_OFF);
-        led_engine_LPUMP.off();
-        log("Left pump set to OFF");
-    }
-    else if (btn_engine_LPUMP.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_LPUMP_ON);
-        led_engine_LPUMP.on();
-        log("Left pump set to ON");
-    }
-    // Right Pump
-    btn_engine_RPUMP.update();
-    if (btn_engine_RPUMP.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_RPUMP_OFF);
-        led_engine_LPUMP.off();
-        log("Right pump set to OFF");
-    }
-    else if (btn_engine_RPUMP.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_RPUMP_ON);
-        led_engine_RPUMP.on();
-        log("Right pump set to ON");
-    }
-    // Carb Head
-    btn_engine_CARB.update();
-    if (btn_engine_CARB.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_CARB_OFF);
-        led_engine_CARB.off();
-        log("Carb. heat set to OFF");
-    }
-    else if (btn_engine_CARB.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_CARB_ON);
-        led_engine_CARB.on();
-        log("Carb. heat set to ON");
-    }
-    // Pitot Head
-    btn_engine_PITOT.update();
-    if (btn_engine_PITOT.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_PITOT_OFF);
-        led_engine_PITOT.off();
-        log("Pitot heat set to OFF");
-    }
-    else if (btn_engine_PITOT.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_PITOT_ON);
-        led_engine_PITOT.on();
-        log("Pitot heat set to ON");
-    }}
-    // Anti ice
-    btn_engine_ANTI_ICE.update();
-    if (btn_engine_ANTI_ICE.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_ANTI_ICE_OFF);
-        led_engine_ANTI_ICE.off();
-        log("Anti-ice set to OFF");
-    }
-    else if (btn_engine_ANTI_ICE.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_ANTI_ICE_ON);
-        led_engine_ANTI_ICE.on();
-        log("Anti-ice set to ON");
-    }
+  // Engines
+  // Left Pump
+  btn_engine_LPUMP.update();
+  if (btn_engine_LPUMP.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_LPUMP_OFF);
+    led_engine_LPUMP.off();
+    logger("Left pump set to OFF");
+  }
+  else if (btn_engine_LPUMP.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_LPUMP_ON);
+    led_engine_LPUMP.on();
+    logger("Left pump set to ON");
+  }
+  // Right Pump
+  btn_engine_RPUMP.update();
+  if (btn_engine_RPUMP.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_RPUMP_OFF);
+    led_engine_LPUMP.off();
+    logger("Right pump set to OFF");
+  }
+  else if (btn_engine_RPUMP.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_RPUMP_ON);
+    led_engine_RPUMP.on();
+    logger("Right pump set to ON");
+  }
+  // Carb Head
+  btn_engine_CARB.update();
+  if (btn_engine_CARB.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_CARB_OFF);
+    led_engine_CARB.off();
+    logger("Carb. heat set to OFF");
+  }
+  else if (btn_engine_CARB.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_CARB_ON);
+    led_engine_CARB.on();
+    logger("Carb. heat set to ON");
+  }
+  // Pitot Head
+  btn_engine_PITOT.update();
+  if (btn_engine_PITOT.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_PITOT_OFF);
+    led_engine_PITOT.off();
+    logger("Pitot heat set to OFF");
+  }
+  else if (btn_engine_PITOT.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_PITOT_ON);
+    led_engine_PITOT.on();
+    logger("Pitot heat set to ON");
+  }
 
-    // Landing gear
-    btn_LDG_GEAR.update();
-    if (btn_LDG_GEAR.fell())
-    { // Switch moved to OFF
-        Serial.write(EVENT_LDG_GEAR_DN);
-        led_engine_ANTI_ICE.off(); 
-        led_LDG_GEAR_GREEN_LEFT.on();
-        led_LDG_GEAR_GREEN_CENTER.on();
-        led_LDG_GEAR_GREEN_RIGHT.on();
-        log("Landing gear set to DOWN");
-    }
-    else if (btn_LDG_GEAR.rose())
-    { // Switch moved to ON
-        Serial.write(EVENT_LDG_GEAR_UP);
-        led_LDG_GEAR_RED.off();
-        led_LDG_GEAR_GREEN_LEFT.off();
-        led_LDG_GEAR_GREEN_CENTER.off();
-        led_LDG_GEAR_GREEN_RIGHT.off();
-        log("Landing gear set to UP");
-    }
+  // Anti ice
+  btn_engine_ANTI_ICE.update();
+  if (btn_engine_ANTI_ICE.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_ANTI_ICE_OFF);
+    led_engine_ANTI_ICE.off();
+    logger("Anti-ice set to OFF");
+  }
+  else if (btn_engine_ANTI_ICE.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_ANTI_ICE_ON);
+    led_engine_ANTI_ICE.on();
+    logger("Anti-ice set to ON");
+  }
 
-    // Flaps up
-    btn_FLAPS_UP.update();
-    if (btn_FLAPS_UP.rose() && current_flaps > 0)
-    { // Switch moved to ON
-        led_FLAPS[current_flaps].off();
-        ccurrent_flaps--;
-        led_FLAPS[current_flaps].on();
-        Serial.write(EVENT_FLAPS_DEC);
-        log("FLAPS DECREMENTED");
-    }
+  // Landing gear
+  btn_LDG_GEAR.update();
+  if (btn_LDG_GEAR.fell())
+  { // Switch moved to OFF
+    Serial.write(EVENT_LDG_GEAR_DN);
+    led_engine_ANTI_ICE.off();
+    led_LDG_GEAR_GREEN_LEFT.on();
+    led_LDG_GEAR_GREEN_CENTER.on();
+    led_LDG_GEAR_GREEN_RIGHT.on();
+    logger("Landing gear set to DOWN");
+  }
+  else if (btn_LDG_GEAR.rose())
+  { // Switch moved to ON
+    Serial.write(EVENT_LDG_GEAR_UP);
+    led_LDG_GEAR_RED.off();
+    led_LDG_GEAR_GREEN_LEFT.off();
+    led_LDG_GEAR_GREEN_CENTER.off();
+    led_LDG_GEAR_GREEN_RIGHT.off();
+    logger("Landing gear set to UP");
+  }
 
-    // Flaps down
-    btn_FLAPS_Dn.update();
-    if (btn_FLAPS_DN.rose() && current_flaps < 3)
-    { // Switch moved to ON
-        led_FLAPS[current_flaps].off();
-        ccurrent_flaps++;
-        led_FLAPS[current_flaps].on();
-        Serial.write(EVENT_FLAPS_INC);
-        log("FLAPS INCREMENTED");
-    }
+  // Flaps up
+  btn_FLAPS_UP.update();
+  if (btn_FLAPS_UP.rose() && current_flaps > 0)
+  { // Switch moved to ON
+    led_FLAPS[current_flaps].off();
+    current_flaps--;
+    led_FLAPS[current_flaps].on();
+    Serial.write(EVENT_FLAPS_DEC);
+    logger("FLAPS DECREMENTED");
+  }
+
+  // Flaps down
+  btn_FLAPS_DN.update();
+  if (btn_FLAPS_DN.rose() && current_flaps < 3)
+  { // Switch moved to ON
+    led_FLAPS[current_flaps].off();
+    current_flaps++;
+    led_FLAPS[current_flaps].on();
+    Serial.write(EVENT_FLAPS_INC);
+    logger("FLAPS INCREMENTED");
+  }
 }
 
 void readSerialInputFromComputer()
@@ -338,25 +346,14 @@ void readSerialInputFromSlave()
 
 void updateDisplay()
 {
-    if(update_display)
+  if (update_display)
+  {
+    u8g2.clearBuffer();
+    for (int i = 0 ; i < LOG_DEPTH ; ++i)
     {
-        u8g2.clearBuffer();
-        for(i = 0 ; i < LOG_DEPTH ; i++)
-        {
-            u8g2.drawStr(1 + 16 * i, 1, LOG_STACK[i].c_str());
-        }
-        u8g2.sendBuffer();
-        update_display = 0;
+      u8g2.drawStr(1 + 16 * i, 1, LOG_STACK[i].c_str());
     }
-}
-
-void log(String log)
-{
-    update_display = 1;
-    delete LOG_STACK[0]
-    for(i=0 ; i < LOG_DEPTH - 1 ; i ++)
-    {
-        LOG_STACK[i] = LOG_STACK[i + 1];
-    }
-    LOG_STACK[LOG_DEPTH - 1] = log;
+    u8g2.sendBuffer();
+    update_display = 0;
+  }
 }
