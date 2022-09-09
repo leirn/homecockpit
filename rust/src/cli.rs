47d8f8792efed238fe78373d1af6b0eb56ddd3b8
leirn <laurent@vromman.org>
2022-09-09 11:43:13 +0200

    Add a function to send a message to arduidno for debug purpore

--- rust/src/cli.rs
+++ rust/src/cli.rs
@@ -10,6 +10,7 @@ enum Commands {
     Stop: String::from("stop"),
     ListPorts: String::from("list_serial_ports"),
     SetPort: String::from("set_port"),
+    SendMessage: String::from("send_message"),
     Help: String::from("help"),
 }
 
@@ -23,8 +24,20 @@ pub fn cli() {
             Commands::Start => start(),
             Commands::Stop => stop(),
             Commands::SetPort => {
-                let port = &command[9..].trim(); // TODO : rework probably necessary
-                set_port(port);
+                println!("What serial port do you want to use ?");
+                let mut port = String::new();
+                stdin().read_line(&mut port).unwrap();
+                set_port(port.trim());
+            },
+            Commands::SendMessage => {
+                println!("Enter the message you want to send in hexadecimal format : 0xabcd");
+                let mut msg_code = String::new();
+                stdin().read_line(&mut msg_code).unwrap();
+                trimmed = msg_code.trim();
+                match <T>::from_str_radix(trimmed.strip_prefix("0x").unwrap(), 16).ok() {
+                    Ok(i) => send_message(i),
+                    Err(..) => println!("The format wasn't respected: {}", trimmed),
+                };
             },
             Commands::ListPorts => list_serial_ports(),
             Commands::Help => print_help(),
@@ -52,6 +65,11 @@ fn set_port(&str _port) {
     //! To be implemented
 }
 
+/// Start listening to serial port
+fn send_message(u16 message) {
+    //! To be implemented
+}
+
 /// List serial port
 fn list_serial_ports() {
     let ports = available_ports().expect("Failed to list serial ports");
@@ -65,6 +83,11 @@ fn list_serial_ports() {
 fn print_help() {
     println!("Available commands:");
     println!("    help : print this help text");
+    println!("    start : start listening to serial port");
+    println!("    stop : stop listening to serial port");
+    println!("    list_serial_ports : display a list of serial ports on this computer");
+    println!("    set_port : ask for the serial port to use");
+    println!("    send_message : ask for a specific command to send. For debugging only");
     println!("    quit : quit application");
 }
 
