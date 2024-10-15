2c12005f8c8a0c8860e7edbfcdda0702274eb46d
leirn <laurent@vromman.org>
2024-10-15 17:03:25 +0200

    Modélisation de l'écran radio

---
+++ experiements/screen_demo.py
@@ -0,0 +1,132 @@
+import pygame
+from pygame.locals import *
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LIGHT_GREY = (220, 220, 220)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+GREEN = (0, 255, 0)
+FONT = "fonts/NanumGothicCoding-Regular.ttf"
+SMALL_FONT_SIZE = 16
+MEDIUM_FONT_SIZE = 19
+
+class CommNavData:
+    def __init__(self, name, activ_freq, stby_freq, id, bearing, distance, selected):
+        self.name = name
+        self.activ_freq = activ_freq
+        self.stby_freq = stby_freq
+        self.id = id
+        self.bearing = bearing
+        self.distance = distance
+        self.selected = selected
+
+
+comms = [
+    CommNavData("NAV1", "114,70", "115.65", "RBT", 150, 21, True),
+    CommNavData("NAV2", "115.65", "123.45", "EPR", 132, 15, False),
+    CommNavData("COM1", "120.750", "119.700", "LFPN TWR", 13, 3, False),
+    CommNavData("COM2", "127.480", "123.450", "LFPN ATIS", 52, 2, True),
+]
+
+def display_block_nav(data: CommNavData):
+
+    # Create a 160x95 px buffer
+    rectangle = pygame.Surface((160, 85))
+
+
+    # Add a text to the screen
+    font = pygame.font.Font(FONT, SMALL_FONT_SIZE)
+    text = font.render(data.name, 1, MAGENTA)
+    rectangle.blit(text, (5, 10))
+
+    font = pygame.font.Font(FONT, MEDIUM_FONT_SIZE)
+    text = font.render(f"{data.stby_freq}", 1, WHITE)
+    text = pygame.transform.scale(text, (min(70, text.get_width()), text.get_height()))
+    rectangle.blit(text, (5, 32))
+    text = font.render("⇔", 1, WHITE)
+    text = pygame.transform.scale(text, (10, text.get_height()))
+    rectangle.blit(text, (75, 32))
+    text = font.render(f"{data.activ_freq} ", 1, GREEN)
+    text = pygame.transform.scale(text, (min(70, text.get_width()), text.get_height()))
+    rectangle.blit(text, (85, 32))
+    if data.selected:
+        pygame.draw.rect(rectangle, GREEN , (5, 28, 73, 30), 1, 0)
+
+    font = pygame.font.Font(FONT, SMALL_FONT_SIZE)
+    text = font.render(data.id, 1, WHITE)
+    rectangle.blit(text, (100, 60))
+
+    font = pygame.font.Font(FONT, SMALL_FONT_SIZE)
+    text = font.render(f"{data.bearing}° {data.distance}NM", 1, MAGENTA)
+    rectangle.blit(text, (5, 60))
+
+    # Add rounded squre to the screen
+    pygame.draw.rect(rectangle, GREEN if data.selected else LIGHT_GREY, (1, 1, 158, 83), 1, 5)
+
+    return rectangle
+
+def display_block_com(data: CommNavData):
+
+    # Create a 160x95 px buffer
+    rectangle = pygame.Surface((160, 85))
+
+
+    # Add a text to the screen
+    font = pygame.font.Font(FONT, SMALL_FONT_SIZE)
+    text = font.render(data.name, 1, MAGENTA)
+    rectangle.blit(text, (105, 10))
+
+    font = pygame.font.Font(FONT, MEDIUM_FONT_SIZE)
+    text = font.render(f"{data.activ_freq}", 1, WHITE)
+    text = pygame.transform.scale(text, (min(70, text.get_width()), text.get_height()))
+    rectangle.blit(text, (5, 32))
+    text = font.render("⇔", 1, WHITE)
+    text = pygame.transform.scale(text, (10, text.get_height()))
+    rectangle.blit(text, (75, 32))
+    text = font.render(f"{data.stby_freq}", 1, WHITE)
+    text = pygame.transform.scale(text, (min(70, text.get_width()), text.get_height()))
+    rectangle.blit(text, (85, 32))
+    if data.selected:
+        pygame.draw.rect(rectangle, GREEN , (83, 28, 72, 30), 1, 0)
+
+    font = pygame.font.Font(FONT, SMALL_FONT_SIZE)
+    text = font.render(data.id, 1, WHITE)
+    rectangle.blit(text, (5, 60))
+
+    font = pygame.font.Font(FONT, SMALL_FONT_SIZE)
+    text = font.render(f"{data.bearing}° {data.distance}NM", 1, MAGENTA)
+    rectangle.blit(text, (90, 60))
+
+    # Add rounded squre to the screen
+    pygame.draw.rect(rectangle, GREEN if data.selected else LIGHT_GREY, (1, 1, 158, 83), 1, 5)
+
+    return rectangle
+
+pygame.init()
+
+# Set up the screen
+screen = pygame.display.set_mode((320, 170))
+
+# Fill the screen with black
+screen.fill(BLACK)
+rectangle = display_block_nav(comms[0])
+screen.blit(rectangle, (0, 0))
+rectangle = display_block_nav(comms[1])
+screen.blit(rectangle, (0, 85))
+rectangle = display_block_com(comms[2])
+screen.blit(rectangle, (160, 0))
+rectangle = display_block_com(comms[3])
+screen.blit(rectangle, (160, 85))
+
+
+
+continuer = True
+while continuer:
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+
+pygame.quit()
